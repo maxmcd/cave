d9df184dd4d844d76d24184734d97c0d5181106a
maxmcd <max.t.mcdonnell@gmail.com>
2020-10-24 12:24:15 -0400

    erase everything and try a different design patern

--- cave.go
+++ cave.go
@@ -1,50 +1,36 @@
 package cave
 
-import (
-	"crypto/rand"
-	"fmt"
-	"net/http"
-
-	"github.com/maxmcd/cave/dom"
-)
-
-type Server struct {
-	new      func() Renderable
-	sessions map[string]Renderable
+type Renderer interface {
+	Render(ctx Context, ui UI) (err error)
 }
 
-func newSession() (out string, err error) {
-	b := make([]byte, 32)
-	_, err = rand.Read(b)
-	if err != nil {
-		return
+type Context struct {
+}
+type UI struct {
+}
+
+func DIV(f ...interface{}) func(ctx Context, ui UI) (err error) {
+	return func(ctx Context, ui UI) (err error) {
+
+		return nil
 	}
-	return fmt.Sprintf("%x", b), nil
 }
 
-func (s Server) ListenAndServe(addr string) error {
-	return http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodGet {
-			sessionKey, err := newSession()
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-			renderable := s.new()
-			s.sessions[sessionKey] = renderable
-			node := renderable.Render()
-			fmt.Fprint(w, node.Print())
-			// write node to body
-		} else if req.Method == http.MethodPost {
-			// handle state change
-		}
-	}))
+func Render(ctx Context, ui UI) (err error) {
+	return nil
 }
 
-func New(new func() Renderable) Server {
-	return Server{new: new, sessions: map[string]Renderable{}}
+type Cave struct {
+	renderer Renderer
 }
 
-type Renderable interface {
-	Render() dom.Node
+func (c *Cave) NewHook() Hook {
+	return Hook{}
 }
+
+type Hook struct {
+	v interface{}
+	c *Cave
+}
+
+func (h *Hook) SetValue()
